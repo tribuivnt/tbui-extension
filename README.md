1be1a88472bd09ee3fddc18f1f55fca39c74bc1f
Ethan BUI <e.bui@linkbynet.com>
2021-09-15 08:17:49 +0700

    Update 20210915

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 tbui Extension for Browser
 
 https://www.flaticon.com/free-icon/memory_401535
+
+```
+localStorage.setItem('todo-items','[{"item":"Test","status":0},{"item":"Test","status":1}]')
+
+fetchItems()
+```
--- popup.js
+++ popup.js
@@ -1,17 +1,19 @@
-document.querySelector('.create-todo').addEventListener('click', function(){
-    document.querySelector('.new-item').style.display='block';
-});
+fetchItems();
+
+// document.querySelector('.create-todo').addEventListener('click', function(){
+//     document.querySelector('.new-item').style.display = "block";
+// });
 
 document.querySelector('.new-item button').addEventListener('click', function(){
     var itemName = document.querySelector('.new-item input').value;
     if (itemName != '') {
         var itemsStorage = localStorage.getItem('todo-items');
-        var itemsArr = JSON.stringify(items);
+        var itemsArr = JSON.parse(itemsStorage);
         itemsArr.push({"item": itemName, "status": 0});
         saveItems(itemsArr);
         fetchItems();
-        document.querySelector('.new-item input').value='';
-        document.querySelector('.new-item').style.display='none';
+        document.querySelector('.new-item input').value = '';
+        document.querySelector('.new-item').style.display = "none";
     }
 });
 
@@ -20,8 +22,9 @@ function fetchItems() {
     itemsList.innerHTML = '';
     var newItemHTML = '';
     try {
-        var itemsStorage = localStorage.getItem('todo-items');
-        var itemsArr = JSON.parse(itemsStorage);
+        var items = localStorage.getItem('todo-items');
+        var itemsArr = JSON.parse(items);
+
         for (var i = 0; i < itemsArr.length; i++) {
             var status = '';
             if(itemsArr[i].status == 1) {
@@ -30,21 +33,26 @@ function fetchItems() {
             newItemHTML += `<li data-itemindex="${i}" ${status}>
             <span class="item">${itemsArr[i].item}</span>
             <div>
-                <span class="itemComplete"><i class="bi bi-check-lg"></i></span>
-                <span class="itemDelete"><i class="bi bi-x-lg"></i></span>
+                <span class="itemComplete"><img src="/images/add.png" alt="add" width="16px"></span>
+                <span class="itemUndo"><img src="/images/delete.png" alt="delete" width="16px"></span>
+                <span class="itemDelete"><img src="/images/bin.png" alt="bin" width="16px"></span>
             </div>
             </li>`;
         }
 
         itemsList.innerHTML = newItemHTML;
-        
         var itemsListUL = document.querySelectorAll('ul li');
         
-        for (var i = 0; i < itemListUL.length; i++) {
+        for (var i = 0; i < itemsListUL.length; i++) {
             itemsListUL[i].querySelector('.itemComplete').addEventListener('click', function() {
                 //
                 var index = this.parentNode.parentNode.dataset.itemindex;
                 itemComplete(index);
+            });
+            itemsListUL[i].querySelector('.itemUndo').addEventListener('click', function() {
+                //
+                var index = this.parentNode.parentNode.dataset.itemindex;
+                itemUndo(index);
             });
             itemsListUL[i].querySelector('.itemDelete').addEventListener('click', function() {
                 //
@@ -68,9 +76,20 @@ function itemComplete(index) {
     document.querySelector('ul.todo-items li[data-itemindex="'+index+'"]').className='done';
 }
 
+function itemUndo(index) {
+    var itemsStorage = localStorage.getItem('todo-items');
+    var itemsArr = JSON.parse(itemsStorage);
+
+    itemsArr[index].status = 0;
+
+    saveItems(itemsArr);
+
+    document.querySelector('ul.todo-items li[data-itemindex="'+index+'"]').className='undo';
+}
+
 function itemDelete(index) {
     var itemsStorage = localStorage.getItem('todo-items');
-    var itemsArr = JSON.parse(items);
+    var itemsArr = JSON.parse(itemsStorage);
 
     itemsArr.splice(index, 1);
 
